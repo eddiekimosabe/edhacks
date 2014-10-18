1a791f89721607c9dca567c44ceef5a69d12ca4c
alanflorendo <alan.florendo@gmail.com>
2014-10-18 13:31:35 -0400

    add migration for links

---
+++ edhacks/db/migrate/20141018173046_create_links.rb
@@ -0,0 +1,11 @@
+class CreateLinks < ActiveRecord::Migration
+  def change
+    create_table :links do |t|
+    	t.string :title
+    	t.string :language
+    	t.string :url
+
+      t.timestamps
+    end
+  end
+end
