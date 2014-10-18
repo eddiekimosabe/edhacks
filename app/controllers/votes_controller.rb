1dc99e9b1b16ce247b8929104763c5d6a00c734f
alanflorendo <alan.florendo@gmail.com>
2014-10-18 16:27:15 -0400

    add functions for votes with polymorphic voting

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,25 @@
 class VotesController < ApplicationController
 
+  private
+  def vote_params
+    params.require(:vote).permit(:user_id, :voteable_id)
+  end
+
+  def voteable_is_comment?
+    params[:comment_id] ? true : false
+  end
+
+  def context_obj
+    voteable_is_comment? ? Comment.find(params[:comment_id]) : Link.find(params[:link_id])
+  end
+
+  def context_type
+    voteable_is_comment? ? "Comment" : "Link"
+  end
+
+  def context_path
+      voteable_is_comment? ? link_path(context_obj.link) : link_path(context_obj)
+  end
 
 
 end
