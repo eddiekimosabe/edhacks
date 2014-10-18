8731762f1e800066aed0ec24c775948fc19258b2
eddiekimosabe <eddiekimosabe@gmail.com>
2014-10-18 15:12:25 -0400

    Setup comments controller
    Add create method for comments
    Add comments_params method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+	def create
+		@link = Link.find(params[:link_id])
+		@comment = Comment.new(comment_params)
+		@comment.user_id = current_user.id 
+		@comment.link_id = @link.id
+
+		if @comment.save
+			redirect_to @link
+		else
+			render :template =>  'links/show'
+		end
+	end
+
+	private
+	def comment_params
+		params.require(:comment).permit(:text, :vote_count)
+	end
 end
