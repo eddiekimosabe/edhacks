dc94a55ac1f0b3d8479bbda3ec61c7a5c870ded3
alanflorendo <alan.florendo@gmail.com>
2014-10-18 14:30:05 -0400

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+languages = %w(ruby python java javascript html css)
+
+5.times do
+  User.create(email: Faker::Internet.email, password: "password")
+end
+
+User.all.each do |user|
+  10.times do
+  	lang_id = rand(languages.length-1) 
+    user.links << Link.create(title: Faker::Lorem.sentence, url: Faker::Internet.url, language: languages[lang_id], vote_count: rand(300))
+  end
+end
+
+Link.all.each do |link|
+	rand(8).times do
+		userid = rand(10)+1
+		Comment.create(user_id: userid, link_id: link.id, text: Faker::Lorem.sentence, vote_count: rand(200))
+	end
+end
