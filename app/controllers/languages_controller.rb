004eec628f2df6e1e822e820de82f734006ea1bf
alanflorendo <alan.florendo@gmail.com>
2014-10-19 13:11:19 -0400

    order links on languages pages so they show in order

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -2,7 +2,7 @@ class LanguagesController < ApplicationController
 
 	def show
 		@language = Language.find(params[:id])
-		@links = Link.where(language: @language)
+		@links = Link.where(language: @language).order("vote_count DESC")
 	end
 
 end
